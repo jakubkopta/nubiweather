2c56a6c92673ce48443a44017e4d5c226660a24c
Jakub Kopta <jakubkopta@gmail.com>
2024-10-10 02:21:54 +0200

    environment variable apiKey

--- spring + kotlin/nubiweather/src/main/kotlin/com/nubisoft/nubiweather/services/NubiweatherService.kt
+++ spring + kotlin/nubiweather/src/main/kotlin/com/nubisoft/nubiweather/services/NubiweatherService.kt
@@ -1,17 +1,21 @@
 package com.nubisoft.nubiweather.services
 
 import com.nubisoft.nubiweather.models.WeatherResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @Service
-class NubiweatherService(private val webClient: WebClient.Builder) {
+class NubiweatherService(
+    private val webClient: WebClient.Builder,
+    @Value("\${weather.api.key}") val apiKey: String
+) {
 
     fun getWeatherData(location: String): Mono<WeatherResponse> {
         return webClient.build()
             .get()
-            .uri("https://api.weatherapi.com/v1/forecast.json?key=e7123ddddd204057a36113613242509&q=$location&days=7&aqi=no&alerts=no")
+            .uri("https://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=$location&days=7&aqi=no&alerts=no")
             .retrieve()
             .bodyToMono(WeatherResponse::class.java)
     }
